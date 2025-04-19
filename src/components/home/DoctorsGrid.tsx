896e89a1933a3b33b9a3ef6c37285ef40fb8d135
bassel <basselahmed007@gmail.com>
2025-04-19 19:24:20 +0200

    deleting not needed file

--- src/components/home/DoctorsGrid.tsx
+++ src/components/home/DoctorsGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import DoctorCard from "../DoctorCard";
 import { doctors } from "../../mock/doctors";
 import { DoctorTypes } from "../../types";
